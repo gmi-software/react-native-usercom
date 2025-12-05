1d27cd8ab2541ba43e8e8f8afa26449d64dcc25f
Łukasz Szpanelewski <l.szpanelewski@gmi.pl>
2025-12-05 15:51:30 +0100

    chore: add user.com maven repo and dependecy as the module plugin

--- module/android/src/main/java/com/margelo/nitro/usercom/HybridUserComModule.kt
+++ module/android/src/main/java/com/margelo/nitro/usercom/HybridUserComModule.kt
@@ -3,11 +3,13 @@ package com.margelo.nitro.usercom
 import android.util.Log
 import androidx.annotation.Keep
 import com.facebook.proguard.annotations.DoNotStrip
+import com.user.sdk.UserCom
 
 @Keep
 @DoNotStrip
 class HybridUserComModule : HybridUserComModuleSpec() {
     override fun initialize() {
+        val instance = UserCom.getInstance()
         Log.d("HybridUserComModule", "Initialized successfully")
     }
 }
